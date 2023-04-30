c0db4bfecf126c88579656c0bef83fc0b748c721
Al Rifat <alrifat088@gmail.com>
2023-04-30 12:23:44 -0400

    task 3 complete

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc : number,
+  price_def : number,
+  ratio : number,
+  upper_bound : number,
+  lower_bound : number,
+  trigger_alert : number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]) {
+    const price_abc = (serverRespond[0].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const price_def = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = price_abc / price_def;
+    const upper_bound = 1 + 0.10;
+    const lower_bound = 1 - 0.10;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc,
+        price_def,
+        ratio,
+        timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+        upper_bound,
+        lower_bound,
+        trigger_alert: (ratio > upper_bound || ratio > lower_bound) ? ratio : undefined,
       };
-    })
   }
 }
